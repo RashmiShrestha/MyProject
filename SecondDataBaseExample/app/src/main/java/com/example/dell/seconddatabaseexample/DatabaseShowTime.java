369efa425146ed20d65e986d018cf2ad09c3abac
RASHMI <RASHMI>
2017-04-13 10:46:36 +0545

    my newproject

---
+++ SecondDataBaseExample/app/src/main/java/com/example/dell/seconddatabaseexample/DatabaseShowTime.java
@@ -0,0 +1,13 @@
+package com.example.dell.seconddatabaseexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DatabaseShowTime extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database_show_time);
+    }
+}
